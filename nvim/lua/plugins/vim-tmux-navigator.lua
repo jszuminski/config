823dc801c4e9e3cff50340b4a55900a7904864ea
jszuminski <jakubszuminski0@gmail.com>
2025-09-28 08:03:24 +0200

    claude code support within tmux

--- nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,19 +1,14 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
-  },
-  keys = {
-    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-  },
+	"christoomey/vim-tmux-navigator",
+	init = function()
+		-- we'll provide our own mappings because of Claude Code workaround
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
+	keys = {
+		{ "<C-h>", "<Cmd>TmuxNavigateLeft<CR>", mode = { "n", "t" }, desc = "Tmux left" },
+		{ "<C-j>", "<Cmd>TmuxNavigateDown<CR>", mode = { "n", "t" }, desc = "Tmux down" },
+		{ "<C-k>", "<Cmd>TmuxNavigateUp<CR>", mode = { "n", "t" }, desc = "Tmux up" },
+		{ "<C-l>", "<Cmd>TmuxNavigateRight<CR>", mode = { "n", "t" }, desc = "Tmux right" },
+		{ "<C-\\>", "<Cmd>TmuxNavigatePrevious<CR>", mode = { "n", "t" }, desc = "Tmux prev" },
+	},
 }
-
