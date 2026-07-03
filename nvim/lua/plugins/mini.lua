9c862e43b012ec70a80ce0575620a2ed2b26ee99
jszuminski <jszuminski@pm.me>
2026-07-03 17:42:06 +0200

    nvim: add mini.ai treesitter textobjects
    vaf/vif (function), vac/vic (class), vao/vio (loop/conditional), plus
    mini.ai defaults: a (argument), quotes, brackets, and g[ / g] edge
    jumps. Query files come from nvim-treesitter-textobjects (master, to
    match nvim-treesitter's branch), used only for its textobjects.scm.

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -5,6 +5,32 @@ return {
     opts = {},
   },
   {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    dependencies = {
+      -- Only used for its textobjects.scm query files, which the
+      -- treesitter specs below read. Branch matches nvim-treesitter's.
+      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "master" },
+    },
+    opts = function()
+      local ai = require("mini.ai")
+      return {
+        n_lines = 300,
+        custom_textobjects = {
+          -- vaf/vif = around/inside function def, vac/vic = class,
+          -- vao/vio = loop or conditional block. Defaults still provide
+          -- a (argument), quotes, and brackets. g[ / g] jump to edges.
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }),
+          o = ai.gen_spec.treesitter({
+            a = { "@conditional.outer", "@loop.outer" },
+            i = { "@conditional.inner", "@loop.inner" },
+          }),
+        },
+      }
+    end,
+  },
+  {
     "echasnovski/mini.surround",
     keys = {
       { "gsa", desc = "Add surrounding", mode = { "n", "v" } },
